0bb1e4d68d7b770698644a4e30f57dc49a263aed
Kanav Gulati <kanavg@gmail.com>
2020-12-08 14:43:13 +0530

    Bug fixes

--- Present-App/PresentApp.py
+++ Present-App/PresentApp.py
@@ -17,53 +17,51 @@ except:
 def run_app():
     global app_start
     user32 = ctypes.windll.user32
-    while app_start == 1:
-        time.sleep(1)
-        user32.keybd_event(0x29, 0, 0x0002, 0)
-        time.sleep(1)
-        user32.keybd_event(0x29, 0, 0, 0)
-    return
+    user32.keybd_event(0x29, 0, 0x0002, 0)
+    time.sleep(1)
+    user32.keybd_event(0x29, 0, 0, 0)
+    if app_start == 1:
+        status.after(1000, func=run_app)
+    else:
+        app_stopped()
+
+def app_stopped():
+    status.config(text="Stopped")
 
 # Function to start the app on button click
 def start_app():
     global app_start
-    #status.config(text="Working")
     app_start = 1
-    run_app()
+    status.config(text="Working")
+    status.after(1000, func=run_app)
     return
 
 # Function to stop the app on button click
 def stop_app():
     global app_start
     app_start = 0
-    #status.config(text="Ready")
+    status.config(text="Ready")
     return
 
 # Main GUI code
-def main():
-    root = tk.Tk()
-    root.geometry('240x240')
-    root.title('Present v1')
-    root.resizable(False, False)
+root = tk.Tk()
+root.geometry('240x240')
+root.title('Present v1')
+root.resizable(False, False)
 
-    mainFrame = tk.Frame(root, bg='gray')
-    mainFrame.pack(fill="both", expand=True)
+mainFrame = tk.Frame(root, bg='gray')
+mainFrame.pack(fill="both", expand=True)
 
-    status = tk.Label(root, text="Use it well!", relief='sunken', anchor=tk.W, bd=1)
-    status.pack(side="bottom", fill="x")
+status = tk.Label(root, text="Use it well!", relief='sunken', anchor=tk.W, bd=1)
+status.pack(side="bottom", fill="x")
 
-    controlBox = tk.Frame(mainFrame)
-    controlBox.pack(padx=10, pady=(10, 0), fill="both", expand=True)
+controlBox = tk.Frame(mainFrame)
+controlBox.pack(padx=10, pady=(10, 0), fill="both", expand=True)
 
-    startButton = tk.Button(controlBox, text="START", width=10, font=font.Font(family='Calibri', size=12, weight='bold'),
-                          command=start_app)
-    startButton.pack(side="left", padx=10, pady=10, expand=True)
+startButton = tk.Button(controlBox, text="START", width=10, font=font.Font(family='Calibri', size=12, weight='bold'),command=start_app)
+startButton.pack(side="left", padx=10, pady=10, expand=True)
 
-    stopButton = tk.Button(controlBox, text="STOP", width=10,
-                            font=font.Font(family='Calibri', size=12, weight='bold'), command=stop_app)
-    stopButton.pack(side="right", padx=10, pady=10, expand=True)
+stopButton = tk.Button(controlBox, text="STOP", width=10, font=font.Font(family='Calibri', size=12, weight='bold'), command=stop_app)
+stopButton.pack(side="right", padx=10, pady=10, expand=True)
 
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
+root.mainloop()
